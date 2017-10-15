8c8e5fecf123d2807521b029ff26b28d1681e0e7
Christian Llanos <challme28@gmail.com>
2017-10-15 18:13:06 -0500

    warning fixed. Can't add value to list

--- src/app.jsx
+++ src/app.jsx
@@ -12,7 +12,7 @@ class Todo extends React.Component {
 class TodoForm extends React.Component {
     render() {
         return (
-            <form onSubmit={() => this.props.onSubmit}>
+            <form onSubmit={this.props.onSubmit}>
                 <label>
                     To do:
                     <input type="text" name="todo" value={this.props.value} onChange={this.props.onChange}/>
@@ -25,11 +25,11 @@ class TodoForm extends React.Component {
 
 export default class App extends React.Component {
 
-    constructor(){
+    constructor() {
         super();
         this.state = {
             value: '',
-            todoList:[]
+            todoList: ['hola','chau']
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -37,8 +37,9 @@ export default class App extends React.Component {
     }
 
     handleSubmit(event) {
-        alert(this.state.value);
-        todoList.push(this.state.value);
+        this.setState({
+            todoList: todoList.concat(this.state.value)
+        });
         event.preventDefault();
     }
 
@@ -46,6 +47,7 @@ export default class App extends React.Component {
         console.log(event.target.value);
         this.setState({value: event.target.value});
     }
+
     render() {
         const todoList = this.state.todoList;
         const todos = todoList.map((todo) => {
@@ -57,9 +59,9 @@ export default class App extends React.Component {
             <div>
                 <div>
                     <TodoForm
-                        value = {this.state.value}
-                        onSubmit={() => this.handleSubmit}
-                        onChange={() => this.handleChange(event)}
+                        value={this.state.value}
+                        onSubmit={(event) => this.handleSubmit(event)}
+                        onChange={(event) => this.handleChange(event)}
                     />
                 </div>
                 <div>{todos}</div>
