5fe383c28032a36ce93f4990c5f0af65344aac40
Christian Llanos <challme28@gmail.com>
2017-10-15 17:56:05 -0500

    Warning, changing from controlled to uncontrolled at input

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,69 @@
 import React from 'react';
 
-export default class App extends React.Component {
+
+class Todo extends React.Component {
     render() {
         return (
+            <div>{this.props.value}</div>
+        )
+    }
+}
+
+class TodoForm extends React.Component {
+    render() {
+        return (
+            <form onSubmit={() => this.props.onSubmit}>
+                <label>
+                    To do:
+                    <input type="text" name="todo" value={this.props.value} onChange={this.props.onChange}/>
+                </label>
+                <input type="submit" value="submit"/>
+            </form>
+        )
+    }
+}
+
+export default class App extends React.Component {
+
+    constructor(){
+        super();
+        this.state = {
+            value: '',
+            todoList:[]
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        alert(this.state.value);
+        todoList.push(this.state.value);
+        event.preventDefault();
+    }
+
+    handleChange(event) {
+        console.log(event.target.value);
+        this.setState({value: event.target.value});
+    }
+    render() {
+        const todoList = this.state.todoList;
+        const todos = todoList.map((todo) => {
+            return (
+                <li key={todo}>{todo}</li>
+            )
+        });
+        return (
             <div>
-                <h1>Hello World</h1>
+                <div>
+                    <TodoForm
+                        value = {this.state.value}
+                        onSubmit={() => this.handleSubmit}
+                        onChange={() => this.handleChange(event)}
+                    />
+                </div>
+                <div>{todos}</div>
+
             </div>
         )
     }
